81288dda0c0e1f20b098de4e287984c799de988c
Muna Nwugo <munachimsonwugo@gmail.com>
2026-09-02 13:52:56 -0400

    cleaning up

--- src/main.rs
+++ src/main.rs
@@ -3,50 +3,26 @@ mod shar;
 mod tests;
 mod types;
 
-use std::sync::Arc;
-use std::thread;
-use std::{path::PathBuf, sync::mpsc};
-
-use shar::error;
+use std::{path::PathBuf, sync::Arc};
 
 use axum::routing::get;
-use clap::{Parser, Subcommand};
+use tokio::sync::RwLock;
+use tower::ServiceBuilder;
+use tower_http::cors::CorsLayer;
+
 use socketioxide::{
     SocketIo,
     extract::{Data, SocketRef, State},
 };
 
-use crate::shar::error::Error;
-use crate::shar::prelude::PeerIdSize;
 use crate::{
-    shar::core::buffer::SharBuffer,
-    types::{Connect, NetworkAdd, NetworkRemove},
+    shar::core::queue::SharQueue,
+    shar::error::Error,
+    shar::prelude::PeerIdSize,
+    types::{Connect, IdeAdd, IdeRemove, NetworkAdd, NetworkRemove},
 };
-use crate::{shar::core::queue::SharQueue, types::IdeAdd};
-use crate::{shar::core::tree::SharDirectory, types::IdeRemove};
-use tokio::sync::RwLock;
-use tower::ServiceBuilder;
-use tower_http::cors::CorsLayer;
-
-use pollster::block_on;
-
-/// Shar CLI
-#[derive(Parser, Debug)]
-#[command(version, about, long_about = None)]
-struct Shar {
-    #[command(subcommand)]
-    command: SharCommand,
-}
 
 /// Defines available Shar Commands
-#[derive(Subcommand, Debug)]
-#[command(arg_required_else_help(true))]
-enum SharCommand {
-    Init {
-        session_id: u32,
-        directory_path: String,
-    },
-}
 
 /// Wraps the queue for use across rooms
 #[derive(Clone, Default)]
@@ -93,13 +69,6 @@ impl QueueWrap {
 
 #[tokio::main]
 async fn main() {
-    println!("main started");
-    let cmd = Shar::parse();
-
-    let input;
-    let (input_tx, input_rx) = mpsc::channel();
-    let output;
-
     // set up web server
     let (layer, io) = SocketIo::builder()
         // provides the state that is the queue and tree to the server
@@ -125,48 +94,6 @@ async fn main() {
         .unwrap();
 
     axum::serve(listener, app).await.unwrap();
-
-    match cmd.command {
-        SharCommand::Init {
-            session_id,
-            directory_path,
-        } => {
-            let shar_init = block_on(initialize_shar(session_id, directory_path));
-
-            match shar_init {
-                Ok((dir, que, buff)) => {
-                    // launch threads
-                    input = thread::spawn(move || {
-                        let queue = que;
-                        let tree = dir;
-
-                        let mut num_received = 0;
-
-                        while num_received < 5 {
-                            let received = input_rx.recv().unwrap();
-                            println!("{}", received);
-                            num_received += 1;
-                        }
-                    });
-
-                    output = tokio::spawn(async move {
-                        let buffer = buff;
-                    });
-                }
-
-                Err(e) => {
-                    eprintln!("Something went wrong during initialization: {}", e);
-                }
-            }
-        }
-    }
-
-    for i in 0..10 {
-        if i % 2 == 0 {
-            input_tx.send("hi").unwrap();
-            println!("message sent");
-        }
-    }
 }
 
 async fn on_connect(socket: SocketRef) {
@@ -201,12 +128,12 @@ async fn on_connect(socket: SocketRef) {
             let add_attempt = queue.add_ide_operation(data).await;
 
             match add_attempt {
-                Ok(_packet) => {
-                    // let _ = socket.within("network").emit("network-add", &packet).await;
+                Ok(packet) => {
+                    let _ = socket.within("network").emit("network-add", &packet).await;
                 }
 
-                Err(_e) => {
-                    // let _ = socket.within("local").emit("ide-add-failed", &e).await;
+                Err(e) => {
+                    let _ = socket.within("local").emit("ide-add-failed", &e).await;
                 }
             }
         },
@@ -218,15 +145,15 @@ async fn on_connect(socket: SocketRef) {
             let remove_attempt = queue.remove_ide_operation(data).await;
 
             match remove_attempt {
-                Ok(_packet) => {
-                    // let _ = socket
-                    //     .within("network")
-                    //     .emit("network-remove", &packet)
-                    //     .await;
+                Ok(packet) => {
+                    let _ = socket
+                        .within("network")
+                        .emit("network-remove", &packet)
+                        .await;
                 }
 
-                Err(_e) => {
-                    // let _ = socket.within("local").emit("ide-remove-failed", &e).await;
+                Err(e) => {
+                    let _ = socket.within("local").emit("ide-remove-failed", &e).await;
                 }
             }
         },
@@ -239,25 +166,4 @@ fn network_add_callback(_row: usize, _col: usize) {
 
 fn network_remove_callback(_row: usize, _col: usize, _is_line: bool) {
     println!("network_remove_callback reached");
-}
-// supporting functions
-async fn initialize_shar(
-    session_id: u32,
-    directory_path: String,
-) -> Result<(SharDirectory, SharQueue, SharBuffer), error::Error> {
-    let _ = session_id;
-
-    let mut counter: u32 = 0;
-    let dir = SharDirectory::new(PathBuf::from(directory_path.clone()), &mut counter)?;
-
-    let buff = SharBuffer::new().await?;
-
-    let queue = SharQueue::new(
-        PathBuf::from(directory_path),
-        1,
-        network_add_callback,
-        network_remove_callback,
-    )?;
-
-    Ok((dir, queue, buff))
 }
